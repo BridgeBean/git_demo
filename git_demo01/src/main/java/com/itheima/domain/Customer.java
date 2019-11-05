e0e50d0de66c5f0c7b512b87295ebb55de45a826
Bridge <416580967@qq.com>
2019-11-05 20:06:31 +0800

    测试分支

---
+++ git_demo01/src/main/java/com/itheima/domain/Customer.java
@@ -0,0 +1,17 @@
+package com.itheima.domain;
+
+/**
+ * @ClassName Customer
+ * @Description TODO
+ * @Author Bridge
+ * @Date 2019/11/5 20:05
+ * @Version 1.0
+ **/
+
+public class Customer {
+    private String name;
+
+}
+
+    
+    
