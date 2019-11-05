cae33782a03b250f3c12c39c52eaea15e77a1e9c
anna <416580967@qq.com>
2019-11-05 19:29:49 +0800

    git连接测试

---
+++ git_demo01/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,18 @@
+package com.itheima.domain;
+
+/**
+ * @ClassName User
+ * @Description TODO git 测试
+ * @Author Bridge
+ * @Date 2019/11/5 19:25
+ * @Version 1.0
+ **/
+
+public class User {
+
+    private String name;
+
+}
+
+    
+    
